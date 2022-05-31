0d4e0ec02d4cb0bc042cc5563d99352e698cc957
rikutoman12 <rikutoo1312@gmail.com>
2022-05-31 06:08:50 +0000

    削除機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+     def new
+         @user = User.new
+     end
+
+    def show
+    end
+
+    def update
+    end
+
 end
--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -2,36 +2,39 @@ class WinesController < ApplicationController
   def new
     @wine = Wine.new
   end
-
+  
   def create
     @wine = Wine.new(wine_params)
     @wine.save
     redirect_to wines_path
   end
-
+  
   def index
     @wines = Wine.all
   end
-
+  
   def show
     @wine = Wine.find(params[:id])
   end
-
+  
   def edit
     @wine = Wine.find(params[:id])
   end
-
+  
   def update
     @wine = Wine.find(params[:id])
     @wine.update(wine_params)
     redirect_to wine_path(@wine)
   end
-
+  
   def destroy
+    @wine = Wine.find(params[:id])
+    @wine.destroy
+    redirect_to wines_path
   end
-
+  
   private
-
+  
   def wine_params
     params.require(:wine).permit(:image,:colour,:comment,:age,:origin,:scent,:taste,:kinds)
   end
